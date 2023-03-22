10a006f490d078c7a46ae24580f1cd827ef3cc20
Armand226 <ouedard18@gmail.com>
2023-03-22 12:16:39 -0500

    backend setup

---
+++ backend.tf
@@ -0,0 +1,20 @@
+resource "aws_s3_bucket" "mybucket" {
+  bucket = "${var.bucket_name}"
+  
+}
+
+resource "aws_kms_key" "mykey" {
+  description             = "This key is used to encrypt bucket objects"
+  deletion_window_in_days = 10
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "encrypt" {
+  bucket = aws_s3_bucket.mybucket.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.mykey.arn
+      sse_algorithm     = "aws:kms"
+    }
+  }
+}
---
+++ var.tf
@@ -0,0 +1,4 @@
+variable "bucket_name" {
+  type = string
+  default = "bootcamp29-018-mybucket"
+}
