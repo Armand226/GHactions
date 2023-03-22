b3e442ae295c5f30d0ee386294c7d603adb44bd7
Armand226 <ouedard18@gmail.com>
2023-03-22 13:25:43 -0500

    seventh commit

--- var.tf
+++ var.tf
@@ -1,4 +1,24 @@
-variable "bucket_name" {
-  type = string
-  default = "bootcamp29-018-mybucket"
+variable "name" {
+  type    = string
+  default = "Armand"
+}
+
+variable "acl" {
+  type    = string
+  default = "private"
+}
+
+variable "versioning" {
+  type    = string
+  default = "Enabled"
+}
+
+variable "region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "create_bucket" {
+  type    = bool
+  default = true
 }
